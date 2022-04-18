cefab5bcdbd71e7bf73d8ed764b594b5996ebdff
greek-zzf <49605925+greek-zzf@users.noreply.github.com>
2022-04-18 13:03:57 +0000

    Update at 2022-04-18T13:03:57.700489Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(compare(123, null));
     }
 
-    public static boolean compare(int a, Integer b) {
+    public static boolean compare(int a, int b) {
         return a == b;
     }
 }
