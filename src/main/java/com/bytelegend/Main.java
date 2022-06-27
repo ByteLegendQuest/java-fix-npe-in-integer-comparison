584ed7a747b14782ab3f83d326607954a99ef083
Anlyui <1391469813@qq.com>
2022-06-27 12:44:01 +0000

    Update at 2022-06-27T12:44:01.400844Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(compare(123, 456));
         System.out.println(compare(123, 123));
-        System.out.println(compare(123, 123));
+        System.out.println(compare(123, null));
     }
 
     public static boolean compare(int a, Integer b) {
+        if (b == null) {
+            return false;
+        }
         return a == b;
     }
 }
