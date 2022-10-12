438d99bd82b395f0583d1aef5f1e6eb296646ea7
tiger0961 <29515547@qq.com>
2022-10-12 06:30:30 +0000

    Update at 2022-10-12T06:30:30.720511Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -8,6 +8,6 @@ public class Main {
     }
 
     public static boolean compare(int a, Integer b) {
-        return a == b.intValue();
-    }
+        int i = integer != null ? integer.intValue() : -1;
+        return a == i;
 }
