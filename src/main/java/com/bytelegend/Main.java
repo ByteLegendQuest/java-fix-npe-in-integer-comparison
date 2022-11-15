10a47bedc27b9fa3cdbd9b72aa892f043a769e6e
Newelle <wozi1988@proton.me>
2022-11-15 10:49:04 +0000

    Update at 2022-11-15T10:49:04.453754Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(compare(123, 456));
         System.out.println(compare(123, 123));
-        System.out.println(compare(123, 0));
+        System.out.println(compare(123, 12));
     }
 
     public static boolean compare(int a, Integer b) {
