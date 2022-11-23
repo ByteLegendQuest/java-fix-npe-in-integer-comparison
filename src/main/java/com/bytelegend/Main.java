01eb6029c463ea6336697a43f6ac2f1ca3465dc1
williamgl <ligan@oregonstate.edu>
2022-11-23 19:50:04 +0000

    Update at 2022-11-23T19:50:04.305751Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -8,7 +8,7 @@ public class Main {
     }
 
     public static boolean compare(int a, Integer b) {
-        if ((a == null) || (b == null)) return false;
+        if (b == null) return false;
         return a == b;
     }
 }
