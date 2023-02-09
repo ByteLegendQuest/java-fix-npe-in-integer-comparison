c4c2a04ca8831db89249dbdbdc2640ad627afe1a
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-09 17:45:43 +0800

    Update Main.java

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(compare(123, null));
     }
 
-    public static boolean compare(int a, Integer b) {
+    public static boolean compare(Integer a, Integer b) {
         return a == b;
     }
 }
