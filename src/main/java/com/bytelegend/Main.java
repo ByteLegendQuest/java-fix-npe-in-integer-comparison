2a2321590c5f577c14777eba83edf5d85ff6a86e
Rakeess <damienaubriet94@gmail.com>
2023-05-24 10:33:03 +0000

    Update at 2023-05-24T10:33:03.177115Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(compare(123, 456));
         System.out.println(compare(123, 123));
-        System.out.println(compare(123, null));
+        System.out.println(compare(123, 10));
     }
 
     public static boolean compare(int a, Integer b) {
